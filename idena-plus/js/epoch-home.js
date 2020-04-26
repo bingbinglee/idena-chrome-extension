29989aa9d57f332516cba1f1a7064b5062b6c705
bingbinglee <supriyo789.next@gmail.com>
2020-04-26 18:03:32 -0400

    Add identities % by epoch

--- idena-plus/js/epoch-home.js
+++ idena-plus/js/epoch-home.js
@@ -3,7 +3,7 @@ var lastepoch = parent2.querySelector(".section__group:first-child");
 
 var parent3 = document.querySelector(".col-sm-6:last-child");
 var firstepoch = parent3.querySelector(".section__group:first-child");
-
+var details = '';
 
 var current = '';
 var genesis = 60;
@@ -76,8 +76,88 @@ window.onload = (function(){
     
     lastepoch.innerHTML += '<hr><div class="control-label">Growth since last epoch: </div><div class="text_block"> '+growth_last+'</div>';
     firstepoch.innerHTML += '<hr><div class="control-label">Growth since first epoch('+first+'): </div><div class="text_block"> '+growth_first+'</div>';
+
+    details = document.querySelector(".section_details");
+
+    	ajax_get('https://api.idena.org/api/Epoch/'+(getUrlVars()["epoch"]-1)+'/IdentityStatesSummary', function(data2) {  
+    		console.log(data2);
+
+    		details.innerHTML = '<section class="section section_details">'+details.innerHTML
+    		+'</section>'
+    		+'<section class="section section_info">'
+    		+'<div class="row">'
+    		
+	    		+'<div class="col-3 col-sm-3">'
+	    			+'<h1>Humans</h1>'
+	    				+'<div class="card">'
+	    					+'<div class="info_block">'
+	    						+'<div class="col-12 col-sm-12 bordered-col">'
+	    							+'<div class="col-auto">'
+	    								+'<h3 class="info_block__accent">'+precise2(data2["result"][0]["count"]/(data2["result"][0]["count"]+data2["result"][1]["count"]+data2["result"][2]["count"])*100)+'%</h3>'
+	    								+'<div class="control-label">'+data2["result"][0]["count"]+' out of '+current+'</div>'
+	    							+'</div>'
+	    						+'</div>'
+	    					+'</div>'
+	    				+'</div>'
+	    		+'</div>'
+
+	    		+'<div class="col-3 col-sm-3">'
+	    			+'<h1>Verified</h1>'
+	    				+'<div class="card">'
+	    					+'<div class="info_block">'
+	    						+'<div class="col-12 col-sm-12 bordered-col">'
+	    							+'<div class="col-auto">'
+	    								+'<h3 class="info_block__accent">'+precise2(data2["result"][1]["count"]/(data2["result"][0]["count"]+data2["result"][1]["count"]+data2["result"][2]["count"])*100)+'%</h3>'
+	    								+'<div class="control-label">'+data2["result"][1]["count"]+' out of '+current+'</div>'
+	    							+'</div>'
+	    						+'</div>'
+	    					+'</div>'
+	    				+'</div>'
+	    		+'</div>'
+
+
+	    		+'<div class="col-3 col-sm-3">'
+	    			+'<h1>Newbie</h1>'
+	    				+'<div class="card">'
+	    					+'<div class="info_block">'
+	    						+'<div class="col-12 col-sm-12 bordered-col">'
+	    							+'<div class="col-auto">'
+	    								+'<h3 class="info_block__accent">'+precise2(data2["result"][2]["count"]/(data2["result"][0]["count"]+data2["result"][1]["count"]+data2["result"][2]["count"])*100)+'%</h3>'
+	    								+'<div class="control-label">'+data2["result"][2]["count"]+' out of '+current+'</div>'
+	    							+'</div>'
+	    						+'</div>'
+	    					+'</div>'
+	    				+'</div>'
+	    		+'</div>'
+
+
+	    		+'<div class="col-3 col-sm-3">'
+	    			+'<h1>Identities Killed</h1>'
+	    				+'<div class="card">'
+	    					+'<div class="info_block">'
+	    						+'<div class="col-12 col-sm-12 bordered-col">'
+	    							+'<div class="col-auto">'
+	    								+'<h3 class="info_block__accent" id="killed">--</h3>'
+	    								+'<div class="control-label">Killed this epoch</div>'
+	    							+'</div>'
+	    						+'</div>'
+	    					+'</div>'
+	    				+'</div>'
+	    		+'</div>'
+
+    		
+    		+'</div>'
+        	+'</section>';
+
+	        	ajax_get('https://api.idena.org/api/Epoch/'+prevEpoch+'/Identities/Count?states[]=Undefined', function(data3) {
+	        		document.getElementById("killed").innerHTML = precise2(data3["result"]);
+	        	});
+
+    	});
     
     });
+
+    
 
   });
 
