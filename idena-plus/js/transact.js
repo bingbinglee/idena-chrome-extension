9c15c45ec625ce37e8feed420983fc42c5f5a8ec
bingbinglee <supriyo789.next@gmail.com>
2020-04-29 10:15:53 -0400

    fix back button problem

--- idena-plus/js/transact.js
+++ idena-plus/js/transact.js
@@ -1,18 +1,49 @@
 var parent2 = document.querySelector(".main .container:last-child");
 var topheader = parent2.querySelector(".section:first-child");
-//console.log(modechange);
 
-topheader.innerHTML += '<div class="button-group">'
-        +'<a href="#" onclick="javascript:history.go(-1);" class="btn btn-secondary btn-small" id="EpochPageLink">'
-         +'<i class="icon icon--back"></i>'
-          +'<span>Back</span>'
-        +'</a>'
-        +'</div>';
+var trgt = '';
+
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
 
 
 window.onload = (function(){
 
+	ajax_get('https://api.idena.org/api/Transaction/'+getUrlVars()["tx"], function(data) {
+
+	trgt = data["result"]["from"];
+
+	topheader.innerHTML += '<div class="button-group">'
+        +'<a href="https://scan.idena.io/address?address='+trgt+'" class="btn btn-secondary btn-small" id="AddressPageLink">'
+         +'<i class="icon icon--back"></i>'
+          +'<span>Back</span>'
+        +'</a>'
+        +'</div>';
+    });
+
 })();
-
-
-
