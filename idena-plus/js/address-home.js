8340f3efe610484ed3e5c82fa27179e32375c514
bingbinglee <supriyo789.next@gmail.com>
2020-05-14 15:56:48 -0400

    fix address and global js

--- idena-plus/js/address-home.js
+++ idena-plus/js/address-home.js
@@ -1,7 +1,11 @@
 var parent2 = document.querySelector(".main .container:last-child");
 var modechange = parent2.querySelector(".section_info");
 
-var address = document.getElementById("Address");
+var address = document.querySelector(".section_main__subtitle");
+
+var str = window.location.href.split( "/address/" );
+var address_str = str[1].split("#");
+
 //console.log(modechange);
 
 var balance = '';
@@ -12,7 +16,7 @@ var txCount = '';
 
 
 function copyFunction() {
-  var copyText = document.getElementById("Address");
+  var copyText = address_str[0];
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
@@ -64,15 +68,6 @@ modechange.innerHTML = details;
 
 
 
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
-
-
 function ajax_get(url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -118,39 +113,39 @@ function round2(x) {
 
 function exchange_id()
 {
-if(getUrlVars()["address"]=='0x83B8A2F9Bd1bE7f1fB496ec91eB0C6983cD38725'){
+if(address_str[0]=='0x83B8A2F9Bd1bE7f1fB496ec91eB0C6983cD38725'){
     
- address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Qtrade.io exchange</span>';
+ address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Qtrade.io exchange</span>';
 
-} else if(getUrlVars()["address"]=='0x1f96445BE4D941934277CCe0C02D9EA8c9cc02f5'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Qbtc exchange</span>';
+} else if(address_str[0]=='0x1f96445BE4D941934277CCe0C02D9EA8c9cc02f5'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Qbtc exchange</span>';
 
-} else if(getUrlVars()["address"]=='0xcbb98843270812eeCE07BFb82d26b4881a33aA91'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Foundation wallet</span>';
+} else if(address_str[0]=='0xcbb98843270812eeCE07BFb82d26b4881a33aA91'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Foundation wallet</span>';
 
-} else if(getUrlVars()["address"]=='0x0000000000000000000000000000000000000000'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Zero Wallet</span>';
+} else if(address_str[0]=='0x0000000000000000000000000000000000000000'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Zero Wallet</span>';
 
-} else if(getUrlVars()["address"]=='0x0a8B4b113d863c86f64E49a1270F7a4A9B65dAAc'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">2020 Funding wallet</span>';
+} else if(address_str[0]=='0x0a8B4b113d863c86f64E49a1270F7a4A9B65dAAc'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">2020 Funding wallet</span>';
 
-} else if(getUrlVars()["address"]=='0x9bf19e7d58B2A95aaBD0cB8bd0Bc7da1c72E696b'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Investor wallet 1</span>';
+} else if(address_str[0]=='0x9bf19e7d58B2A95aaBD0cB8bd0Bc7da1c72E696b'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Investor wallet 1</span>';
 
-} else if(getUrlVars()["address"]=='0x5b8896aEd1d98604c00bAcF1643F752949Fe807D'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Investor wallet 2</span>';
+} else if(address_str[0]=='0x5b8896aEd1d98604c00bAcF1643F752949Fe807D'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Investor wallet 2</span>';
 
-} else if(getUrlVars()["address"]=='0xc94D32638D71aBA05F0bDADE498948eF93944428'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Ambassador fund</span>';
+} else if(address_str[0]=='0xc94D32638D71aBA05F0bDADE498948eF93944428'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Ambassador fund</span>';
 
-} else if(getUrlVars()["address"]=='0xCe12D460d2dd8786f409Ae7E5AE43dB8d9C6Dc29'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">Team funds distribution wallet</span>';
+} else if(address_str[0]=='0xCe12D460d2dd8786f409Ae7E5AE43dB8d9C6Dc29'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">Team funds distribution wallet</span>';
 
-} else if(getUrlVars()["address"]=='0x477E32166cd16C1b4909BE783347e705Aef3d5db'){
-address.innerHTML = getUrlVars()["address"]+'<span class="badge badge-secondary">2021-2022 Funding Wallet</span>';
+} else if(address_str[0]=='0x477E32166cd16C1b4909BE783347e705Aef3d5db'){
+address.innerHTML = address_str[0]+'<span class="badge badge-secondary">2021-2022 Funding Wallet</span>';
 
 } else {
-    address.innerHTML = getUrlVars()["address"];
+    address.innerHTML = '<span id="Address">'+address_str[0]+'</span>';
 
 }
 var topheader = parent2.querySelector(".section:first-child");
@@ -168,7 +163,7 @@ topheader.innerHTML += '<div class="button-group">'
         +'</div>';
 
 
-var qr = '<div class="qr"><img src="https://chart.apis.google.com/chart?cht=qr&amp;chs=150x150&amp;chl='+getUrlVars()["address"]+'&amp;chld=H|0"></div>';
+var qr = '<div class="qr"><img src="https://chart.apis.google.com/chart?cht=qr&amp;chs=150x150&amp;chl='+address_str[0]+'&amp;chld=H|0"></div>';
 
 topheader.innerHTML += qr;
 }
@@ -176,7 +171,7 @@ topheader.innerHTML += qr;
 
 window.onload = (function(){
 
-  ajax_get('https://api.idena.io/api/Address/'+getUrlVars()["address"], function(data) {
+  ajax_get('https://api.idena.io/api/Address/'+address_str[0], function(data) {
     //console.log(data);
     
     balance = precise2(data['result']['balance']);
--- idena-plus/js/global.js
+++ idena-plus/js/global.js
@@ -2,63 +2,54 @@
 var imgURL = chrome.extension.getURL("idena-logo-dark.svg");
 var parent = document.querySelector(".header_logo");
 
-parent.querySelector("a img").src = imgURL;
 var mode = localStorage.getItem('mode') || '';
-
-
-
-var parent2 = document.querySelector(".justify-content-between");
-var modechange = parent2.querySelector(".col");
-
 
 
 document.getElementsByClassName('header')[0].classList.add('fixed-header');
 document.getElementsByClassName('main')[0].classList.add('extra-main-padding');
 
-var scrolltop = '<button type="button" class="btn btn-icon hide" id="scrollTop" onclick="window.scrollTo({ top: 0, behavior: \'smooth\' });">'
-				+'<i class="icon icon--thin_arrow_up"></i></button>';
-
-var themeTop = '<span id="themeTop" class="hide">'
-			+'<button type="button" id="moon" class="rem btn btn-icon theme"'
-						+' onclick="localStorage.setItem(\'mode\', \'dark\');'
-            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\''+imgURL+'\');'
-            +'document.getElementsByTagName(\'html\')[0].classList.add(\'darkmode\');'
-            +'document.getElementById(\'moon\').classList.add(\'rem\');'
-            +'document.getElementById(\'sun\').classList.remove(\'rem\');'
-						+'" id="moon">'
-						+'<img src="'+chrome.extension.getURL("images/moon.svg")+'" width="18px" height="18px"' 
-						+'style="opacity: 0.8;"/></button>'
-            +'<button type="button" id="sun" class="rem btn btn-icon theme"'
-						+' onclick="localStorage.setItem(\'mode\', \'light\'); document.getElementsByTagName(\'html\')[0].classList.remove(\'darkmode\');'
-            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\'https://scan.idena.io/images/idena-logo.svg\');'
-            +'document.getElementById(\'moon\').classList.remove(\'rem\');'
-            +'document.getElementById(\'sun\').classList.add(\'rem\');'
-						+'" id="sun">'
-						+'<img src="'+chrome.extension.getURL("images/sun.svg")+'" width="18px" height="18px"' 
-						+'style="opacity: 0.8;"/></button>'
-						+'</span>'; 
-
-
-if(document.querySelector(".section:last-child")){
-document.querySelector(".section:last-child").innerHTML = document.querySelector(".section:last-child").innerHTML + scrolltop + themeTop;
-}
-
-/* modechange.innerHTML += htmcontent; */
-
-
-if (mode =='dark') {
-  parent.querySelector("a img").src = imgURL;
-  document.getElementById('sun').classList.remove('rem');
-
-} else {
-  parent.querySelector("a img").src = 'https://scan.idena.io/images/idena-logo.svg';
-  document.getElementById('moon').classList.remove('rem');
-} 
-
-
 var sticky = document.getElementsByClassName('header')[0].offsetTop;
 
-window.onscroll = function() {shadowFunction()};
+
+	scrolltop = '<button type="button" class="btn btn-icon hide" id="scrollTop" onclick="window.scrollTo({ top: 0, behavior: \'smooth\' });">'
+					+'<i class="icon icon--thin_arrow_up"></i></button>'
+				+'<span id="themeTop" class="hide">'
+				+'<button type="button" id="moon" class="rem btn btn-icon theme"'
+							+' onclick="localStorage.setItem(\'mode\', \'dark\');'
+	            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\''+imgURL+'\');'
+	            +'document.getElementsByTagName(\'html\')[0].classList.add(\'darkmode\');'
+	            +'document.getElementById(\'moon\').classList.add(\'rem\');'
+	            +'document.getElementById(\'sun\').classList.remove(\'rem\');'
+							+'" id="moon">'
+							+'<img src="'+chrome.extension.getURL("images/moon.svg")+'" width="18px" height="18px"' 
+							+'style="opacity: 0.8;"/></button>';
+
+	themetop = '<button type="button" id="sun" class="rem btn btn-icon theme"'
+							+' onclick="localStorage.setItem(\'mode\', \'light\'); document.getElementsByTagName(\'html\')[0].classList.remove(\'darkmode\');'
+	            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\'https://scan.idena.io/static/images/idena-logo.svg\');'
+	            +'document.getElementById(\'moon\').classList.remove(\'rem\');'
+	            +'document.getElementById(\'sun\').classList.add(\'rem\');'
+							+'" id="sun">'
+							+'<img src="'+chrome.extension.getURL("images/sun.svg")+'" width="18px" height="18px"' 
+							+'style="opacity: 0.8;"/></button>'
+							+'</span>'; 
+
+	document.getElementsByTagName('body')[0].innerHTML += themetop+scrolltop;
+
+	if (mode =='dark') {
+	  parent.querySelector("a img").src = imgURL;
+	  document.getElementById('sun').classList.remove('rem');
+
+	} else {
+	  parent.querySelector("a img").src = 'https://scan.idena.io/static/images/idena-logo.svg';
+	  document.getElementById('moon').classList.remove('rem');
+	} 
+
+
+window.onscroll = function() {
+	shadowFunction()
+};
+
 
 function shadowFunction() {
   if (window.pageYOffset > sticky) {
